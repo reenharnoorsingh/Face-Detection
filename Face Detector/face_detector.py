2701c52d8ce30ca80bfd6a0bb444c5f1cb4df72d
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2020-10-15 22:40:50 +0530

    make the image grayscale

--- Face Detector/face_detector.py
+++ Face Detector/face_detector.py
@@ -6,7 +6,13 @@ trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #choose an image to detect images
 img = cv2.imread('image.jpg')
 
-cv2.imshow('Face Detector Harnoor',img)
+#convert image to grayscale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+
+
+cv2.imshow('Face Detector Harnoor', grayscaled_img)
 cv2.waitKey()
 
 print("Code Completed")
