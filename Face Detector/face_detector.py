c1de9e02f318b7c7a16e0a895471f3d9abe73d29
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2020-10-15 23:01:51 +0530

    Update face_detector.py

--- Face Detector/face_detector.py
+++ Face Detector/face_detector.py
@@ -1,20 +1,25 @@
 import cv2
 
-#loads the trained algorithm for face detection
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# loads the trained algorithm for face detection
+trained_face_data = cv2.CascadeClassifier(
+    'haarcascade_frontalface_default.xml')
 
-#choose an image to detect images
+# choose an image to detect images
 img = cv2.imread('image.jpg')
 
-#convert image to grayscale
+# convert image to grayscale
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#detect faces
+# detect faces
 face_points = trained_face_data.detectMultiScale(grayscaled_img)
 print(face_points)
 
+# draw rectangles
+for (x, y, w, h) in face_points:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-cv2.imshow('Face Detector Harnoor', grayscaled_img)
+
+cv2.imshow('Face Detector Harnoor', img)
 cv2.waitKey()
 
 print("Code Completed")
