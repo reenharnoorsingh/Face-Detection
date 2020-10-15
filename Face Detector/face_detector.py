89f2db11d288e75daa4211c5eaeeac65b7d0d8d5
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2020-10-15 21:35:55 +0530

    image addition

--- Face Detector/face_detector.py
+++ Face Detector/face_detector.py
@@ -1,6 +1,10 @@
 import cv2
 
+#loads the trained algorithm for face detection
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#choose an image to detect images
+img = cv2.imread('image.jpg')
 
 
 print("Code Completed")
