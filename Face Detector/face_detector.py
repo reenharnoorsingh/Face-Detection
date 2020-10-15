2e7489ce37e022140f2e83dfffb9306a35cab9b8
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2020-10-15 21:42:14 +0530

    Update face_detector.py

--- Face Detector/face_detector.py
+++ Face Detector/face_detector.py
@@ -6,5 +6,7 @@ trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #choose an image to detect images
 img = cv2.imread('image.jpg')
 
+cv2.imshow('Face Detector Harnoor',img)
+cv2.waitKey()
 
 print("Code Completed")
